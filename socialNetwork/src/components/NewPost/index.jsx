a2444b95504cfa80b7e2ee326b6d6accff48640b
Nicholas mendes <98132580+Nicholasmendespereira@users.noreply.github.com>
2023-03-20 23:15:32 -0300

    up

--- socialNetwork/src/components/NewPost/index.jsx
+++ socialNetwork/src/components/NewPost/index.jsx
@@ -1,19 +1,18 @@
-import { ContainerMain, BoxOne, BoxTwo, BoxThree } from './style'
+import { ContainerMain, BoxOne, BoxTwo, BoxThree } from "./style";
+
+//Recat icons
+import { BiWorld, BiMessageRounded } from "react-icons/bi";
+import { IoMdSettings } from "react-icons/io";
 
 function NewPost() {
   return (
-  <>
-    <ContainerMain>
-    <BoxOne>
-.
-    </BoxOne>
-    <BoxTwo>
-.
-    </BoxTwo>
-    <BoxThree>
-.
-    </BoxThree>
-    </ContainerMain>
-</>
-)}
+    <>
+      <ContainerMain>
+        <BoxOne>.</BoxOne>
+        <BoxTwo>.</BoxTwo>
+        <BoxThree><BiWorld/><BiMessageRounded/><IoMdSettings/></BoxThree>
+      </ContainerMain>
+    </>
+  );
+}
 export default NewPost;
